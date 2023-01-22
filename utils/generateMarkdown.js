120b96e97a54a3a38a9567b554eb7bfa618bdc7c
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-01-22 10:42:24 +0000

    added badge() to generate a badge based on the users choice of license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,22 @@
 // function to generate markdown for README
+function badge(data) {
+  const licenseType = data.license[0];
+  let licenseString = ""
+  if (licenseType === "MIT") {
+    licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  };
+  if (licenseType === "GNU General Public License 2.0") {
+    licenseString = `![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)`
+  };
+  if (licenseType === "Apache License 2.0") {
+     licenseString = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
+    };
+  if (licenseType === "GNU General Public License 3.0") {
+      licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+    };
+  return licenseString
+  };
+
 function generateMarkdown(data) {
   return `# ${data.title}
 
@@ -6,7 +24,7 @@ function generateMarkdown(data) {
 ${data.description} 
 
   ## Table of Contents:
-    1. [Description](#description) 
+    1. [Description](#Description) 
     2. [Installation](#Installation)
     3. [Usage](#Usage)  
     4. [License](#License)
@@ -21,7 +39,7 @@ ${data.installation}
 ${data.usage}
 
 ## License
-${licenseBadge(data)}
+${badge(data)}
 
 ## Contributing
 ${data.contributing}
