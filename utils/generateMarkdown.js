75b978d521814eb73bcd4039fbc3ee376d5742e7
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-01-22 11:02:02 +0000

    table of contents links now work

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,17 +1,17 @@
 // function to generate markdown for README
 function badge(data) {
   const licenseType = data.license[0];
-  let licenseString = ""
-  if (licenseType === "MIT") {
+  let licenseString = ''
+  if (licenseType === 'MIT') {
     licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
   };
-  if (licenseType === "GNU General Public License 2.0") {
+  if (licenseType === 'GNU General Public License 2.0') {
     licenseString = `![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)`
   };
-  if (licenseType === "Apache License 2.0") {
+  if (licenseType === 'Apache License 2.0') {
      licenseString = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
     };
-  if (licenseType === "GNU General Public License 3.0") {
+  if (licenseType === 'GNU General Public License 3.0') {
       licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
     };
   return licenseString
@@ -21,16 +21,17 @@ function generateMarkdown(data) {
   return `# ${data.title}
 
 ## Description
-${data.description} 
+> ${data.description} 
 
-  ## Table of Contents:
-    1. [Description](#Description) 
-    2. [Installation](#Installation)
-    3. [Usage](#Usage)  
-    4. [License](#License)
-    5. [Contributing](#Contributing)
-    6. [Tests](#Tests)
-    7. [Contact](#Contact)
+## Table of Contents:
+
+- [Description](#description) 
+- [Installation](#installation)
+- [Usage](#usage)  
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Contact](#contact)
 
 ## Installation
 ${data.installation}
@@ -48,7 +49,8 @@ ${data.contributing}
 ${data.tests}
 
 ## Contact
-${data.questions}`
+${data.questions}
+`
 }
 
 
