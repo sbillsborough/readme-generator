679dcd537f8986849d7f0f9a1b4dc704b3011297
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-01-22 01:09:40 +0000

    added array of questions in the inquier.prompt method

--- utils/promptUser.js
+++ utils/promptUser.js
@@ -3,10 +3,51 @@ const inquirer = require('inquirer');
 function promptUser() {
   inquirer.prompt([
     {
-      type: 'number',
-      name: 'num1',
-      message: 'Enter your first number.'
-    }
+      type: 'string',
+      name: 'title',
+      message: 'Enter the project\'s title:'
+    },
+    {
+      type: 'string',
+      name: 'description',
+      message: 'Enter the project\'s description:'
+    },
+    {
+      type: 'string',
+      name: 'contents-table',
+      message: 'Enter the project\'s contents:'
+    },
+    {
+      type: 'string',
+      name: 'installation',
+      message: 'Enter the project\'s installation instructions:'
+    },
+    {
+      type: 'string',
+      name: 'usage',
+      message: 'Enter a description of how to use your project:'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose the project\'s license type:',
+      choices: ['MIT', 'None']
+    },
+    {
+      type: 'string',
+      name: 'contributing',
+      message: 'Enter the project\'s contributers:'
+    },
+    {
+      type: 'string',
+      name: 'tests',
+      message: 'Enter any tests for your project:'
+    },
+    {
+      type: 'string',
+      name: 'questions',
+      message: 'Enter your contact information for any questions about the project:'
+    },
   ]);
 }
 
