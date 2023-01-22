5996df51185bb284f3b462cf911f1d186addb7f8
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-01-22 10:20:55 +0000

    added the Mark Down to generate to the generateMarkdown function body

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,37 @@
-const inquirer = require('inquirer');
-const promptUser = require('./promptUser')
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
-}
+## Description
+${data.description} 
 
+  ## Table of Contents:
+    1. [Description](#description) 
+    2. [Installation](#Installation)
+    3. [Usage](#Usage)  
+    4. [License](#License)
+    5. [Contributing](#Contributing)
+    6. [Tests](#Tests)
+    7. [Contact](#Contact)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${licenseBadge(data)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Contact
+${data.questions}`
+}
 
 
 module.exports = generateMarkdown;
