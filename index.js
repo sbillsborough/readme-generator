492ae1677fd2ea7451531d41731ba2c8d52df672
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-01-23 04:44:39 +0000

    readded writeToFile function

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const promptUser = require('./utils/promptUser')
 
+
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
 // function to initialize program
 function init() {
   promptUser();
