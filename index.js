4d86775609de9f342747fca83fb293a97b204fe7
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-01-23 04:40:56 +0000

    removed the writeToFile function as it wasn't needed in the index.js

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const promptUser = require('./utils/promptUser')
 
-// array of questions for user
-const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
 // function to initialize program
 function init() {
   promptUser();
